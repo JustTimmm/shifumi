a65c307cddc317eacdd0549e858383d9e604d066
TarzaxDev <98848694+TarzaxDev@users.noreply.github.com>
2024-08-25 22:30:14 +0200

    rock paper scissors ✨

--- main.go
+++ main.go
@@ -3,22 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		age     int
-		name    string
-		student bool
-	)
+	var rock = "rock"
+	var paper = "paper"
+	var scissors = "scissors"
 
-	age = 50
-	name = "Jone"
-	student = true
-
-	fmt.Printf("Bonjour je suis %v ", name)
-	fmt.Printf("j'ai %vans ", age)
-
-	if student == true {
-		fmt.Printf("et je suis actuellement étudiant.")
-	} else {
-		fmt.Printf("et je ne suis pas étudiant.")
-	}
+	fmt.Printf("%v ", rock)
+	fmt.Printf("%v ", paper)
+	fmt.Printf("%v ", scissors)
 }
